d635ccf55457e7c8f004b8d929f37e09e981b1e2
Fabio F Silva <ffsilva@outlook.com>
2021-12-28 21:08:17 +0000

    FizzBuzz challenge

---
+++ python/FizzBuzz.py
@@ -0,0 +1,15 @@
+'''
+Print out 100 numbers, from 1 to 100, but:
+- instead of printing multiples of 3, print 'Fizz'
+- instead of printing multiples of 5, print 'Buzz''
+- instead of printing multiples of both 3 and 5, print 'FizzBuzz'
+'''
+
+for number in range(1, 101):
+    if number % 3 == 0:
+        print('Fizz')
+    if number % 5 == 0:
+        print('Buzz')
+    if number % 3 == 0 and number % 5 == 0:
+        print('FizzBuzz')
+    print(number)
